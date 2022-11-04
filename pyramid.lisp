114e39b543e73e2c708308b4ce5823ac2257b1e2
Juan Antonio Zuloaga Mellino <jzuloaga@fi.uba.ar>
2022-11-04 18:16:54 -0300

    pyramid

--- pyramid.lisp
+++ pyramid.lisp
@@ -1,13 +1,13 @@
 ;https://www.codewars.com/kata/55fd2d567d94ac3bc9000064
 
-(defun pyramid-level (n) 
-  (+ (/ (* (- n 1) n) 2) 1)
-  )
+(defun pyramid-start (n) 
+  (+ (/ (* (- n 1) n) 2) 1))
 
-(defun buddy (n)
-    (loop 
-     :for i from (pyramid-level n) to 
-      :when (setf retval (hasbuddy i)) return (format nil "~a" retval) 
-      :finally (return "Nothing")))
+(pyramid-level 3)
 
-(pyramid-level 5)
+(defun row-sum-odd-numbers (n) 
+    (let ((start (pyramid-start n)))
+      (loop :for i :from start :to (+ start n -1)
+        :summing (+(* (- i 1) 2) 1) )))
+
+(row-sum-odd-numbers 2)
